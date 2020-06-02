57486775e4cac6b8aa7f04bbdbcec41e9d79220b
Faris <farissatyaw@gmail.com>
2020-06-02 22:52:12 +0700

    Ep 11 Done

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <header class="flex items-center mb-3 py-4">
-    <div class="flex w-full justify-between items-center">
+    <div class="flex w-full justify-between items-ends">
         <h2 class="text-sm text-grey-dark font-normal">My Projects</h2>
         <a href="/projects/create" class="button">New Project</a>
     </div>
@@ -10,14 +10,9 @@
 
 <main class="lg:flex lg:flex-wrap -mx-3">
     @forelse($projects as $project)
-        <div class="lg:w-1/3 px-3 pb-6">
-            <div class="bg-white rounded-lg p-5 shadow"style="height:200px">
-                <h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-light pl-4 mb-3">
-                    <a href="{{$project->path()}}" class="text-black no-underline">{{$project->tittle}}</a>
-                </h3>
-                    <div class="text-grey-dark">{{\Illuminate\Support\Str::limit($project->description, 100)}}</div>
-            </div>
-        </div>  
+    <div class="lg:w-1/3 px-3 pb-6">
+        @include('projects.card')
+    </div>
     @empty
         <div>No Projects yet</div>
     @endforelse
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,11 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class='container'>
-        <h1>Birdboard</h1>
-        {{$project -> tittle}}
-        <div>
-            {{$project->description}}
+<header class="flex items-center mb-3 py-4">
+    <div class="flex w-full justify-between items-ends">
+        <p class="text-sm text-grey-dark font-normal">
+           <a href="/projects" class="text-sm text-grey-dark font-normal no-underline"> My Projects </a> / {{$project->tittle}}
+        </p>
+        <a href="/projects/create" class="button">New Project</a>
+    </div>
+</header>
+
+<main class='container'>
+    <div class="lg:flex -mx-3">
+        <div class="lg:w-3/4 px-3 mb-8">
+            <div class="mb-6">
+                <h2 class="text-grey-dark font-normal text-lg mb-3">Tasks</h2>  
+                <div class="card mb-3">Lorem Ipsum.</div>
+                <div class="card mb-3">Lorem Ipsum.</div>
+                <div class="card mb-3">Lorem Ipsum.</div>
+                
+            </div>
+            
+            <div class="mb-6">
+                <h2 class="text-grey-dark font-normal text-lg">General Notes</h2>  
+            </div>
+
+            <textarea class="card w-full" style="min-height:200px">Lorem Ipsum.</textarea>
         </div>
-    </div> 
+        <div class="lg:w-1/4 px-3">
+                @include('projects.card')
+        </div>
+    </div>
+</main> 
 @endsection
